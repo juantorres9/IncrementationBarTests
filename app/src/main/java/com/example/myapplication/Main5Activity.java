0613c5ff14c8a6ce15fab5a2d1df6b2958985b64
jctor <juan.torres.zetino@gmail.com>
2022-11-13 23:31:14 +0100

    adding validation button template

--- app/src/main/java/com/example/myapplication/Main5Activity.java
+++ app/src/main/java/com/example/myapplication/Main5Activity.java
@@ -20,12 +20,19 @@ public static String TAG5=Main5Activity.class.toString();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
         int width_dp=150;
-        int height_dp=250;
+        int height_dp=80;
         int width_pixels=DensityUtil.dip2px(this,width_dp);
         int height_pixels=DensityUtil.dip2px(this,height_dp);
         Log.d(TAG5,"hi the width DP is="+width_dp+ " but the width in PX is ="+width_pixels);
         Log.d(TAG5,"hi the height DP is="+height_dp+ " but the height in PX is ="+height_pixels);
 
+
+        int width_px=120;
+        int height_px=400;
+        int width_dips=DensityUtil.px2dip(this,width_px);
+        int height_dips=DensityUtil.px2dip(this,height_px);
+        Log.d(TAG5,"hi the width_PX is="+width_px+ " but the width in DIPS is ="+width_dips);
+        Log.d(TAG5,"hi the height_PX is="+height_px+ " but the height in DIPS is ="+height_dips);
 //OK CODE
         LayoutInflater inflater = (LayoutInflater)      this.getSystemService(LAYOUT_INFLATER_SERVICE);
       //  RelativeLayout.LayoutParams imageParams = new RelativeLayout.LayoutParams(150  ,200);  //PIXELS PARAMETERS
